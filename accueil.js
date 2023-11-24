b33c9ac63d731b0e83cbd58a7f55c149341263ae
romane <amiot.romane56@gmail.com>
2023-11-24 13:13:00 +0100

    Enlever le math.random

--- accueil.js
+++ accueil.js
@@ -31,31 +31,6 @@ input.addEventListener('keyup', ListResultat);
 
 
 //Créer une fontion qui affiche des pokémons de manière aléatoire
-let container = $("#poke_container")
-
-fetch('https://tyradex.vercel.app/api/v1/pokemon')
-    .then((response) => response.json())
-    .then((pokemons) => showList(pokemons));
- 
-function showList(pokemons) {
-    pokemons.forEach(e => {
-        showPokemon(e)
-    });
-    let random = Math.floor(Math.random() * $('.cart-pokemons').length);
-    $($('.cart-pokemons')[random]).show()
-}
-function showPokemon(poke) {
-    let cart = "<div class='cart-pokemons'>" +
-        "<img class='img' src='" + poke.sprites.regular + "'>" +
-        "<h4 class='nom'>" + poke.name.fr + "</h4>" +
-        "</div>"
- 
-        $(container).append(cart)
-       
-        for (let i=0; i<$('.cart-pokemons').length; i++) {
-            $($('.cart-pokemons')[i]).hide()
-        }}
-
 
 /* 
 //Fonction qui affiche les pokemons 
