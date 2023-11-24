aa25f8f9c233d5f702aefa37b8ab325fb5a0631b
romane <amiot.romane56@gmail.com>
2023-11-24 13:07:34 +0100

    math.random

--- accueil.js
+++ accueil.js
@@ -31,9 +31,30 @@ input.addEventListener('keyup', ListResultat);
 
 
 //Créer une fontion qui affiche des pokémons de manière aléatoire
-/* const pokemonArray = ['https://tyradex.vercel.app/api/v1/gen/1']
-    let generation_1 = pokemonArray.filter((pokemon)=> pokemon.generation =='1' )
-    console.log(generation_1) */ 
+let container = $("#poke_container")
+
+fetch('https://tyradex.vercel.app/api/v1/pokemon')
+    .then((response) => response.json())
+    .then((pokemons) => showList(pokemons));
+ 
+function showList(pokemons) {
+    pokemons.forEach(e => {
+        showPokemon(e)
+    });
+    let random = Math.floor(Math.random() * $('.cart-pokemons').length);
+    $($('.cart-pokemons')[random]).show()
+}
+function showPokemon(poke) {
+    let cart = "<div class='cart-pokemons'>" +
+        "<img class='img' src='" + poke.sprites.regular + "'>" +
+        "<h4 class='nom'>" + poke.name.fr + "</h4>" +
+        "</div>"
+ 
+        $(container).append(cart)
+       
+        for (let i=0; i<$('.cart-pokemons').length; i++) {
+            $($('.cart-pokemons')[i]).hide()
+        }}
 
 
 /* 
