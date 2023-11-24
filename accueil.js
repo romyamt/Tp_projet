04937c3621c8188f384ecceb0010214b98a332f8
romane <amiot.romane56@gmail.com>
2023-11-24 16:11:38 +0100

    MIse en page

--- accueil.js
+++ accueil.js
@@ -63,7 +63,7 @@ function showList(pokemons) {
             class: 'cart-pokemons',
             html: `<h4 class='nom'>${poke.name.fr}</h4>
                 <img class='img' src='${poke.sprites.regular}'>
-                   <h4 class='nom'>${poke.name.fr}</h4>`
+                `
         });
         fragment.appendChild(cart[0]);
     });
@@ -126,11 +126,7 @@ async function showPokemonDetail(pokedexId) {
     <button onclick="goList()"> Revenir à la liste </button>
         <article class="PokemonDetail carte">
         <img class='images' src='`+ pokemon.sprites.regular + `'>
-        <h2> `+ pokemon.name.fr + ` </h2> 
-        <p>${pokemon.types.map(type => {
-            return `<img src="${type.image}" alt="${type.name}"/>`;
-        }).join(' ')}</p>
-        
+        <h2> `+ pokemon.name.fr + ` </h2>         
         </article> 
         `;
         contenu.innerHTML += content
